d0c84bd33c95faea1345cf101289703caa26feca
Martina Acosta <martinaacostamg@gmail.com>
2026-08-27 08:53:13 -0300

    Agregar test de creación de la pieza Palo

---
+++ test/PiezaPalo.test.ts
@@ -0,0 +1,15 @@
+import test from "node:test";
+import assert from "node:assert/strict";
+import { PiezaPalo } from "../src/piezas/PiezaPalo.ts";
+
+test("crear una pieza Palo con su forma", () => {
+  const pieza = new PiezaPalo();
+
+  assert.equal(pieza.nombre, "Palo");
+  assert.deepEqual(pieza.forma, [
+    [1],
+    [1],
+    [1],
+    [1]
+  ]);
+});
