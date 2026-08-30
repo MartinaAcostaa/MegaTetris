9775958fb6b1fb5f4cd8d38d0332f9e14932c6ee
Sophia Saquer <saquersophia@gmail.com>
2026-08-30 19:09:33 -0300

    Agregar test para colocar una pieza en el tablero

--- test/Tablero.test.ts
+++ test/Tablero.test.ts
@@ -1,6 +1,7 @@
 import test from "node:test";
 import assert from "node:assert/strict";
 import { Tablero } from "../src/Tablero.ts";
+import { PiezaT } from "../src/piezas/PiezaT.ts";
 
 test("crear un tablero vacío de 20 filas por 10 columnas", () => {
   const tablero = new Tablero();
@@ -13,5 +14,19 @@ test("crear un tablero vacío de 20 filas por 10 columnas", () => {
   assert.equal(
     tablero.celdas.flat().every((celda) => celda === 0),
     true
+  );
+});
+
+test("agregar una pieza completa dentro del tablero", () => {
+  const tablero = new Tablero();
+  const pieza = new PiezaT();
+
+  tablero.agregarPieza(pieza, 0);
+
+  assert.deepEqual(
+    tablero.celdas
+      .slice(0, 2)
+      .map((fila) => fila.slice(0, 3)),
+    pieza.forma
   );
 });
